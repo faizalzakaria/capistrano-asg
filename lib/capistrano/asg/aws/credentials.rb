4465ad24c90f185f4021ce3e30732f16024ad0b4
Faizal Zakaria <fai@code3.io>
2019-09-18 00:32:38 +0800

    Fallback to environment when profile is missing

--- lib/capistrano/asg/aws/credentials.rb
+++ lib/capistrano/asg/aws/credentials.rb
@@ -9,10 +9,14 @@ module Capistrano
 
         def credentials
           if fetch(:aws_profile)
-            credentials = ::Aws::SharedCredentials.new(profile_name: fetch(:aws_profile)).credentials
-            if credentials
-              set :aws_access_key_id, credentials.access_key_id
-              set :aws_secret_access_key, credentials.secret_access_key
+            begin
+              credentials = ::Aws::SharedCredentials.new(profile_name: fetch(:aws_profile)).credentials
+              if credentials
+                set :aws_access_key_id, credentials.access_key_id
+                set :aws_secret_access_key, credentials.secret_access_key
+              end
+            rescue
+              puts "Failed to retrieve AWS Profile, try env"
             end
           end
 
